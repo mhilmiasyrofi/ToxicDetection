07509229cd44a2822a5e78428665406853531f15
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-18 09:44:19 +0800

    doc: script to download jigsaw dataset

---
+++ input/jigsaw-unintended-bias-in-toxicity-classification/download.sh
@@ -0,0 +1,3 @@
+kaggle competitions download -c jigsaw-unintended-bias-in-toxicity-classification
+unzip jigsaw-unintended-bias-in-toxicity-classification.zip
+rm -rf jigsaw-unintended-bias-in-toxicity-classification.zip
